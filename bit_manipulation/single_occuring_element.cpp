42d008822e3d3cf6ad9dbd5c40e03664ddf7318d
avnigoyal25 <91952706+avnigoyal25@users.noreply.github.com>
2023-01-24 20:53:56 +0530

    Create single_occuring_element.cpp

---
+++ bit_manipulation/single_occuring_element.cpp
@@ -0,0 +1,21 @@
+//example-[1,2,4,2,6,4,1]=6
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int ans=0;
+    for(int i=0;i<n;i++)
+    {
+        ans=ans^a[i];
+    }
+    cout<<ans<<endl;
+}
